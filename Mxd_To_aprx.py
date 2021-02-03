3aae1f5f596315d549ccb5431e5a1dcb514c5ba5
meidad hoze <48644044+medad-hoze@users.noreply.github.com>
2021-02-03 22:54:57 +0200

    Update Mxd_To_aprx.py

--- Mxd_To_aprx.py
+++ Mxd_To_aprx.py
@@ -4,30 +4,42 @@ import os
 
 arcpy.env.overwriteOutput = True
 
-def Mxd_format_to_10_3(MXD_to_convert,temp_aprx):
-    folder      = os.path.dirname(MXD_to_convert)
-    name        = os.path.basename(MXD_to_convert).split('.')[0] + '.aprx'
+def Mxd_format_to_10_3(MXD_to_convert,temp_aprx,temp_gdb):
+    folder      = os.path.dirname(MXD_to_convert[0])
+    name        = os.path.basename(MXD_to_convert[0]).split('.')[0] + '.aprx'
     New_aprx     = folder +'\\'+name
 
     aprx = arcpy.mp.ArcGISProject(temp_aprx)
-    aprx.importDocument(MXD_to_convert)
+    aprx.importDocument(MXD_to_convert[0])
+
+    m = aprx.listMaps('Layers')[0]
+    m.updateConnectionProperties(temp_gdb,MXD_to_convert[1])
     aprx.saveACopy(New_aprx)
 
-def Fix_fodler_raster(Fodler,search_endwith):
-    save_mxd = []
-    already_exists = []
+
+def Fix_fodler_raster(Fodler):
+    save_mxd,save_gdb,= [],[]
+
     for root,dirs,files in os.walk(Fodler):
         for file in files:
-            if file.endswith(search_endwith):
+            if file.endswith('.mxd') and 'Copy' not in file:
                 print (root + '\\' + file)
                 save_mxd.append(root + '\\' + file)
-    return save_mxd
+            if file.endswith('gdb'):
+                gdb_name = os.path.dirname(root + '\\' + file)
+                save_gdb.append(gdb_name)
+                print (gdb_name)
 
 
-save_mxd  = Fix_fodler_raster(r'C:\Users\Administrator\Desktop\medad\python\Work\Mpy_Pro\Source_files','.mxd')
-temp_aprx = r"C:\Users\Administrator\Desktop\medad\python\Work\Mpy_Pro\aprx_temp\Edit89689.aprx"
+    return list(zip(save_mxd,save_gdb))
+
+
+folder    = r'C:\Users\Administrator\Desktop\medad\python\Work\Mpy_Pro\Source_files'
+temp_aprx = r"C:\Users\Administrator\Desktop\medad\python\Work\Mpy_Pro\aprx_temp\EditTazar89689\Edit89689.aprx"
+temp_gdb  = r"C:\Users\Administrator\Desktop\medad\python\Work\Mpy_Pro\aprx_temp\EditTazar89689\CadasterEdit_Tazar.gdb"
+
+save_mxd  = Fix_fodler_raster(folder)
 
 for i in save_mxd:
-    Mxd_format_to_10_3(i)
-
+    Mxd_format_to_10_3(i,temp_aprx,temp_gdb)
 
